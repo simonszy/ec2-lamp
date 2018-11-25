51b35f4e770c5db256236a6a129326239d50f33e
Simons Attipoe <simons.attipoe@gmail.com>
2018-11-24 21:50:11 -0500

    Coniguring ansible to work with dynamic repositories

---
+++ roles/php/files/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head><title>Welcome to DevOps on AWS</title></head>
+    <body>
+        <h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
+
+    </body>
+</html>
