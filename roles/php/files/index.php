bac412c2ed4b520d48d5963a14795afb1d989fef
Simons Attipoe <simons.attipoe@gmail.com>
2018-11-24 22:46:10 -0500

    Testing ansible pull again

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -3,6 +3,6 @@
     <head><title>Welcome to DevOps on AWS</title></head>
     <body>
         <h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
-        <h2><?php echo "This change will be reflected using ansible pull" ?>
+        <h2><?php echo "This change will be reflected using ansible pull again" ?>
     </body>
 </html>
