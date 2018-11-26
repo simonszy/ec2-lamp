a31e960e4e97a2391fe978d4abc79a468530f82e
Simons Attipoe <simons.attipoe@gmail.com>
2018-11-25 23:10:53 -0500

    This is the last commit before log off

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -4,6 +4,7 @@
     <body>
         <h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
         <h2><?php echo "This change will be reflected using ansible pull again" ?>
-        <h2><?php echo "This is a cool technology to know!!!" ?>
+        <h2><?php echo "This is a cool technology to know!!!" ?></h2>
+        <h1><?php echo "This is the last change before log off" ?></h1>
     </body>
 </html>
